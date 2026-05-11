589de3a1434fb12eed878966e4eb673bc8c3178f
Sumedh <sumedh.it@gmail.com>
2026-05-11 17:25:27 +0530

    few changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/site",
@@ -12,5 +12,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
-})
+  ]
+});
